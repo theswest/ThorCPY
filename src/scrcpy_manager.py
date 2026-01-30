c3c4f211086b54e4e1fb011f5be4d65367e2017c
theswest <samueljwest@outlook.com>
2026-01-30 00:35:40 +0000

    Spacing Fix

--- src/scrcpy_manager.py
+++ src/scrcpy_manager.py
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # src/scrcpy_manager.py
+
 import os
 import subprocess
 import time
--- src/ui_pygame.py
+++ src/ui_pygame.py
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # src/ui_pygame.py
+
 import pygame
 import os
 import tkinter as tk
